e15e5ca90cd531b9c6e0b76e80ce317f828619af
James Allen <jacques2186@yahoo.com>
2025-01-08 22:02:56 -0500

    Add categories and update teams view

---
+++ app/Livewire/ViewPlayersFromTeam.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Player;
+use App\Models\Team;
+
+class ViewPlayersFromTeam extends ViewPlayers {
+    protected int $teamId;
+    public string $teamName;
+    public function mount(
+        int $team
+    ) {
+        $this->teamId = $team;
+        $this->teamName = Team::select('name')->firstWhere('id', $this->teamId)->name;
+    }
+
+    public function query()
+    {
+        return Player::query()
+            ->where('team_id', $this->teamId)
+            ->with(['team', 'lastTeam', 'media'])
+            ->where('published_at', '<', now()->endOfDay());
+    }
+}
