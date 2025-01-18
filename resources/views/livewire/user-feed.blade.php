eb126e5cf2d34d5ed2e9f799ff39dd8162c5f1e1
James Allen <jacques2186@yahoo.com>
2025-01-17 21:17:16 -0500

    Use correct profile route

--- resources/views/livewire/user-feed.blade.php
+++ resources/views/livewire/user-feed.blade.php
@@ -1,6 +1,6 @@
 <div class="max-w-5xl mx-auto flex mt-8">
     <aside class="hidden sm:block sm:w-64">
-        <a href="{{ auth()->check() ? route('users.feed', auth()->user()) : route('login') }}">My Feed</a>
+        <a href="{{ auth()->check() ? route('users.profile', auth()->user()) : route('login') }}">My Feed</a>
     </aside>
     <main class="divide-y w-full">
     @foreach ($this->feeds as $feed)
