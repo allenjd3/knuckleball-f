710d68297ba230b6a403f3e694a4aedd0fecd1b4
James Allen <jacques2186@yahoo.com>
2025-01-17 21:39:20 -0500

    Change colors and remove logo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,7 +30,7 @@ class AppServiceProvider extends ServiceProvider
             'danger' => Color::Red,
             'gray' => Color::Zinc,
             'info' => Color::Blue,
-            'primary' => Color::Indigo,
+            'primary' => Color::Red,
             'success' => Color::Green,
             'warning' => Color::Amber,
         ]);
--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,9 @@ use App\Livewire\ViewPlayersFromTeam;
 use App\Livewire\ViewTeams;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', UserFeed::class)->name('users.feed');
 Route::get('teams', ViewTeams::class)->name('teams.index');
 Route::get('teams/{team}', ViewPlayersFromTeam::class)->name('teams.show');
-Route::get('feed', UserFeed::class)->name('users.feed');
 Route::get('feed/{user:slug}', UserProfile::class)->name('users.profile');
 Route::get('players', ViewPlayers::class)->name('players.index');
 Route::get('players/{player}', ShowPlayer::class)->name('players.show');
