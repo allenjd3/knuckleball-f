e8c98b17fc4084a911b98ea984210123db80b55a
James Allen <jacques2186@yahoo.com>
2025-01-30 07:57:40 -0500

    Add to guest as well

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="shortcut icon" href={{ asset('favicon-32x32.png') }} />
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://use.typekit.net">
