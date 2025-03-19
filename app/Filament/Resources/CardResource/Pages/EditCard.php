63376266430d3c7e7374113d2e7f06b244cdc4d2
James Allen <jacques2186@yahoo.com>
2025-03-18 20:34:18 -0400

    Lint

--- app/Filament/Resources/CardResource/Pages/EditCard.php
+++ app/Filament/Resources/CardResource/Pages/EditCard.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\CardResource\Pages;
 
 use App\Filament\Resources\CardResource;
-use Filament\Actions;
+use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditCard extends EditRecord
@@ -13,7 +13,7 @@ class EditCard extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            DeleteAction::make(),
         ];
     }
 }
--- app/Filament/Resources/CardResource/Pages/ListCards.php
+++ app/Filament/Resources/CardResource/Pages/ListCards.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\CardResource\Pages;
 
 use App\Filament\Resources\CardResource;
-use Filament\Actions;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCards extends ListRecords
@@ -13,7 +13,7 @@ class ListCards extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            CreateAction::make(),
         ];
     }
 }
