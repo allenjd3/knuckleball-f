060a62c68727c4da1a88eff12b975d5fdcc2aa46
James Allen <jacques2186@yahoo.com>
2025-03-22 14:58:00 -0400

    Fix fee amount

--- resources/views/livewire/show-player.blade.php
+++ resources/views/livewire/show-player.blade.php
@@ -51,7 +51,7 @@
                 <h3 class="font-bold px-2">Fees:</h3>
                 <div class="flex divide-x divide-black">
                     @forelse ($this->fees as $fee)
-                        <p class="px-2">{{ str($fee->feeMaterial->name)->plural() }} {{ $fee->amount }} per {{ str($fee->feeMaterial->name) }}</p>
+                        <p class="px-2">${{ $fee->amount }} per {{ str($fee->feeMaterial->name) }}</p>
                     @empty
                         <p class="px-2">No fees yet!</p>
                     @endforelse
