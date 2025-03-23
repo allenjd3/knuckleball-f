4ea3b45057f212ae8e07af64e34774c2500475b7
James Allen <jacques2186@yahoo.com>
2025-03-23 16:03:46 -0400

    Clean up duplicate addresses

---
+++ app/Console/Commands/CleanDuplicateAddresses.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Player;
+use Illuminate\Console\Command;
+
+class CleanDuplicateAddresses extends Command
+{
+    protected $signature = 'address:clean-duplicates';
+
+    protected $description = 'Removes duplicate addresses';
+
+    public function handle()
+    {
+        Player::lazy()
+            ->each(
+                fn ($player) => $player->addresses()
+                    ->where('addresses.id', "!=", $player->address->id)
+                    ->delete(),
+            );
+    }
+}
