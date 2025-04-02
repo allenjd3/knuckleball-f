7e9ce964a1510c6615215ada7b8bf4d38bb9531c
James Allen <jacques2186@yahoo.com>
2025-04-01 21:13:55 -0400

    Fix add player bug

--- app/Livewire/ViewPlayersFromTeam.php
+++ app/Livewire/ViewPlayersFromTeam.php
@@ -4,13 +4,18 @@ namespace App\Livewire;
 
 use App\Models\Player;
 use App\Models\Team;
+use Filament\Actions\Action;
+use Filament\Actions\CreateAction;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Model;
 
 class ViewPlayersFromTeam extends ViewPlayers
 {
     public string $teamName;
     public ?string $mediaUrl;
 
-    protected int $teamId;
+    public int $teamId;
 
     public function mount(
         int $team
@@ -27,5 +32,31 @@ class ViewPlayersFromTeam extends ViewPlayers
             ->where('team_id', $this->teamId)
             ->with(['team', 'lastTeam', 'media'])
             ->where('published_at', '<', now()->endOfDay());
+    }
+
+    public function createAction(): Action
+    {
+        return CreateAction::make()
+            ->model(Player::class)
+            ->label(__('New Player'))
+            ->form([
+                TextInput::make('name'),
+                FileUpload::make('url')
+                    ->directory('avatars')
+                    ->nullable()
+                    ->avatar(),
+            ])
+            ->using(function (array $data): Model {
+                $data = collect($data)->merge(['team_id' => $this->teamId]);
+                $player = Player::create($data->only(['name', 'team_id'])->toArray());
+
+                if ($url = $data->get('url')) {
+                    $player->media()->create([
+                        'url' => $url,
+                    ]);
+                }
+
+                return $player;
+            });
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,22 +5,23 @@ namespace Database\Seeders;
 use App\Models\Player;
 use App\Models\Team;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         $user = User::factory()->create([
             'name' => 'James Allen',
             'email' => 'james@example.com',
             'super_admin' => true,
+
+        ]);
+
+        User::factory()->create([
+            'name' => 'Brittany Allen',
+            'email' => 'britt@example.com',
+            'super_admin' => false,
         ]);
 
         $teams = Team::factory(10)
