9ee53fc19373c35006582759692a3cc57502c838
James Allen <jacques2186@yahoo.com>
2025-04-21 09:37:24 -0400

    Update tailwind to v4

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'uppercase inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-custom-600 hover:text-custom-600 focus:outline-none focus:border-red-700 transition duration-150 ease-in-out'
-            : 'uppercase inline-flex items-center px-1 pt-1 border-transparent text-sm font-medium hover:text-custom-600 leading-5 text-gray-500 focus:outline-none focus:text-custom-600 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'uppercase inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-custom-600 hover:text-custom-600 focus:outline-hidden focus:border-red-700 transition duration-150 ease-in-out'
+            : 'uppercase inline-flex items-center px-1 pt-1 border-transparent text-sm font-medium hover:text-custom-600 leading-5 text-gray-500 focus:outline-hidden focus:text-custom-600 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a style="--c-600: var(--primary-600);" {{ $attributes->merge(['class' => $classes]) }}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 import preset from './vendor/filament/support/tailwind.config.preset'
-import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
     presets: [preset],
@@ -8,11 +7,4 @@ export default {
         './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
-    theme: {
-        extend: {
-            fontFamily: {
-                'display': ['industry-inc-base', ...defaultTheme.fontFamily.serif],
-            },
-        }
-    }
 }
