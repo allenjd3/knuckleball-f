70596e64acb4cc6a0d0a5112c2372feb4276437b
James Allen <jacques2186@yahoo.com>
2025-04-27 15:15:59 -0400

    Fix the look of the postal-mail to match current

--- resources/views/components/feeds/postal-mail.blade.php
+++ resources/views/components/feeds/postal-mail.blade.php
@@ -1,11 +1,13 @@
 <article class="py-2 grid grid-cols-[100px,1fr]">
-    <div class="flex items-start justify-center w-[100px]">
+    <div class="flex items-start justify-center items-center w-[100px]">
         <img class="size-12 border rounded-full" src="{{ $photo }}" alt="{{ $user }}" />
     </div>
     <div>
         <p class="font-bold"><a href="{{ $player_path }}">{{ $player }}</a></p>
-        <p class="mb-2"><a href="{{ $user_path }}" class="underline">{{ $user }}</a> sent {{ $type ? $type . " " : "" }}on {{ $dateSent }}</p>
+        <p class="mb-2 text-sm"><a href="{{ $user_path }}" class="underline font-bold">{{ $user }}</a> sent {{ $type ? $type . " " : "" }}on {{ $dateSent }}</p>
         <div>{{ $feed->comment }}</div>
-        <p class="mt-2 font-bold">Returned {{ $dateReturned }}</p>
+        @if ($dateReturned)
+            <p class="mt-2 font-bold">Returned {{ $dateReturned }}</p>
+        @endif
     </div>
 </article>
