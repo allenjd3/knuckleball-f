0b01f141a976e86f6055ffc520b5084b6a93058d
James Allen <jacques2186@yahoo.com>
2025-07-20 21:06:11 -0400

    Add less intense randomization

--- app/Livewire/UserFeed.php
+++ app/Livewire/UserFeed.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Feed;
+use Cache;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\On;
 use Livewire\Component;
@@ -45,7 +46,14 @@ class UserFeed extends Component
                         fn ($query) => $query->selectRaw('0'),
                     ), 'is_following'
             )
-            ->orderByRaw('(is_following * 0.6 + RAND() * 0.4) DESC')
+            ->orderByRaw(
+                "
+                    CASE
+                        WHEN created_at > ? AND followable_id = ? THEN 1
+                        ELSE 0
+                    END DESC
+                ", [now()->subMinutes(30), auth()->user()->id])
+            ->orderByRaw('FIELD(id, ' . $this->getOrderedIds()->implode(',') . ')')
             ->orderByDesc('created_at')
             ->simplepaginate();
     }
@@ -54,5 +62,34 @@ class UserFeed extends Component
     public function updateFeed()
     {
         unset($this->feeds);
+    }
+
+    private function getOrderedIds()
+    {
+        return Cache::flexible('user-feed-order',
+            [900, 3600],
+            fn () =>
+                Feed::query()
+                    ->with('feedable')
+                    ->select('feeds.*')
+                    ->selectSub(
+                        fn ($query) => $query
+                            ->when(
+                                auth()->check(),
+                                fn ($query) => $query->selectRaw('1')
+                                    ->from('users as u')
+                                    ->whereColumn('u.id', 'feeds.followable_id')
+                                    ->where(
+                                        fn ($query) => $query
+                                            ->whereIn('u.id', auth()->user()?->following()->select('users.id'))
+                                            ->orWhere('u.id', auth()->user()?->id)
+                                    ),
+                                fn ($query) => $query->selectRaw('0'),
+                            ), 'is_following'
+                    )
+                    ->orderByRaw('(is_following * 0.95 + RAND() * 0.05) DESC')
+                    ->orderByDesc('created_at')
+                    ->pluck('id')
+        );
     }
 }
