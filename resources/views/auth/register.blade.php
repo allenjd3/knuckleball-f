bd5b8a4a44ba46cca6a2f266e2dfd6033252a668
James Allen <jacques2186@yahoo.com>
2026-05-02 07:21:13 -0400

    Add some margin to turnstile

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -68,7 +68,9 @@
                     </x-label>
                 </div>
             @endif
-            <x-turnstile />
+            <div class="mt-4">
+                <x-turnstile />
+            </div>
 
             <div class="flex items-center justify-end mt-4 gap-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('dmca') }}" target="_blank">
